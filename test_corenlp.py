cdf2d176c436ba4b9b221413648ca67e46c4bf26
Nii Larteh Annan <annanniilarteh@gmail.com>
2025-04-30 10:47:10 +0000

    fixed some errors

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,4 +1,5 @@
-from nlplogic.corenlp import search_wikipedia, summarize_wikipedia, get_text_blob, get_phrases
+from nlplogic.corenlp import get_phrases
+
 
 def test_get_phrases():
-    assert 'real madrid' in get_phrases("Real Madrid")
+    assert "real madrid" in get_phrases("Real Madrid")
